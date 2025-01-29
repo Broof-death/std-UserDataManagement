602fb8bf639154f426a4a86827a32eadf0a441a3
Fernando Gabriel Reyes Vásquez <fdoreyesvasquez@gmail.com>
2025-01-29 19:31:19 -0300

    Carga códigos fuente

---
+++ growerhelper/src/main/java/cl/grower/growerhelper/GrowerhelperApplication.java
@@ -0,0 +1,13 @@
+package cl.grower.growerhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowerhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowerhelperApplication.class, args);
+	}
+
+}
---
+++ growerhelper/src/test/java/cl/grower/growerhelper/GrowerhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.grower.growerhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowerhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
